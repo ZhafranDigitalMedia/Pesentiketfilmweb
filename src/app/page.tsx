52176f0429566ed1c5b61decd9dfc4aa1c68aa95
zhafran14 <80811273+ZhafranDigitalMedia@users.noreply.github.com>
2025-12-14 10:50:21 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,55 +7,26 @@ import Image from "next/image";
 
 import { auth } from "../utils/firebase";
 import logo from "../assets/cinebook-logo.png";
-import MovieGrid from "../components/MovieGrid";
-import HeaderNav from "../components/HeaderNav";
-import AdminHeaderNav from "../components/adminHeaderNav";
-import AddCinema from "../components/addCinema";
-import Footer from "../components/Footer";
 
 export default function HomePage() {
-  const router = useRouter(); // ✅ INI WAJIB
-  const [role, setRole] = useState<string | null>(null);
+  const router = useRouter();
   const [loading, setLoading] = useState(true);
 
- useEffect(() => {
-  const unsub = onAuthStateChanged(auth, (user) => {
-    if (!user) {
-      router.replace("/login");
-      return;
-    }
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        router.replace("/login");
+      } else {
+        setLoading(false);
+      }
+    });
 
-    const role = localStorage.getItem("role") ?? "user";
-    setRole(role);
-    setLoading(false);
-  });
-
-  return () => unsub();
-}, []);
-
+    return () => unsub();
+  }, [router]);
 
   if (loading) {
-    return (
-      <div className="min-h-screen flex justify-center items-center text-white text-2xl">
-        Loading...
-      </div>
-    );
+    return <div className="text-white text-2xl">Loading...</div>;
   }
 
-  return (
-    <div style={{ backgroundColor: "#6A77E0", minHeight: "100vh" }}>
-      <header className="flex justify-center items-center gap-3 p-4">
-        <Image src={logo} alt="CineBook logo" width={100} height={100} />
-        <h1 className="text-4xl font-bold text-white">CineBook</h1>
-      </header>
-
-      {role === "admin" ? <AdminHeaderNav /> : <HeaderNav />}
-
-      <div className="p-8">
-        {role === "admin" ? <AddCinema /> : <MovieGrid />}
-      </div>
-
-      <Footer />
-    </div>
-  );
+  return <div>HOME PAGE</div>;
 }
