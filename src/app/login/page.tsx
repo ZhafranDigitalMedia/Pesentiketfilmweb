57b8c8e376a37b4eb3f4a1dea270492f59c6a2e1
zhafran14 <80811273+ZhafranDigitalMedia@users.noreply.github.com>
2025-12-14 11:59:08 +0700

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -19,7 +19,7 @@ export default function LoginPage() {
   useEffect(() => {
     const unsub = onAuthStateChanged(auth, (user) => {
       if (user) {
-        router.replace("/");
+        router.replace("/profile");
       }
     });
     return () => unsub();
